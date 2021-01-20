8d92ebcbd2d66c02ea04836afa0699d6aebd61a0
juliansyahrifqi <rifqipratama9@gmail.com>
2021-01-21 00:25:21 +0700

    tambahin font sama rapihin home

--- application/views/main.php
+++ application/views/main.php
@@ -1,87 +1,142 @@
-<link rel = "stylesheet" type = "text/css" 
-   href = "<?php echo base_url(); ?>assets/css/styles.css">  
-<div class="col-md-12" style="background-color: #FFB004; border: 0.1px solid #FFB004">
-		<h1 class="banner">Know Your <br> Nutritional Needs <br> With UniHealth</h1>
-		<br>
-		<a class="start" href="#"><button>Get Started &#160;&#160;&#10148;</button></a>
-		<br>
-		<br>
-		<br>
-		<br>
-		<img src="<?= base_url('assets/images/fotobanner.png'); ?>" alt="Foto Banner" style="width: 300px; position: absolute; bottom: 0px; right: 0px;">
-</div>
-		<div class="col-md-12" style="background-color: #fff7de; border: 0.1px solid #fff7de">
-		<div class="card-deck">
-		<div class="card">
-  			<img class="card-img-top" src="...">
-			<div class="card-body">
-			   	<h5 class="card-title" align="center">Fried Rice</h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	<!-- Jumbotron -->
+	<div class="col-lg-12" style="background-color: #FFB004;">
+		<div class="jumbotron py-4 my-0" style="background-color: #FFB004">
+			<div class="row align-items-center">
+				<div class="col-lg-7 col-sm-12">
+					<div class="banner">
+						<i>
+							<h1 class="text-darken font-weight-bold" style="font-size: 70px; font-family: 'Archivo', sans-serif;">
+								Know Your Nutritional Needs With UniHealth
+							</h1>
+						</i>
+						<a href="#" class="btn btn-dark mt-4" style="font-size: 18px; font-family: 'Oswald', sans-serif; font-style: normal;">
+							Get Started <i class="fas fa-play pl-2"></i>
+						</a>
+					</div>
+				</div>
+
+				<div class="col-lg-5 col-sm-12 text-center">
+					<div class="banner-image ">
+						<img src="<?= base_url('assets/images/banner.png'); ?>" alt="Banner Image" style="width: 500px; height: 500px;">
+					</div>
+				</div>
 			</div>
-			  	<ul class="list-group">
-			    	<li class="list-group-item">Main Course &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;1000 Kkal</li>
-			  	</ul>
-			<div class="card-body">
-			    <a style="margin-left: 5px;">Asian </a>
-			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
-  			</div>
 		</div>
+	</div>
+	<!-- End of Jumbotron -->
 
-		<div class="card">
-  			<img class="card-img-top" src="...">
-			<div class="card-body">
-			   	<h5 class="card-title" align="center">Fried Rice</h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	<!-- Food Card -->
+	<div class="col-lg-12 mt-0" style="background-color: #fff7de; border: 0.1px solid #fff7de">
+
+		<div class="row px-3 my-5">
+			<div class="col-lg-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/images/image1.jpg'); ?>" alt="Card image cap">
+					<div class="card-body">
+						<h3 class="card-title text-center">Fried Rice</h3>
+					</div>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-3 py-0" style="font-family: 'Roboto', sans-serif; font-weight: 500; color: #AFAFAF">
+						<p class="food-type mb-0">Main Course</p>
+						<p class="total-calory mb-0">1000 kkal</p>
+					</div>
+
+					<hr>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-4 pt-2 pb-4" style="font-family: 'Roboto', sans-serif; font-weight: 500;">
+						<p class="food-origin mb-0">Asian</p>
+						<a href="#" class="btn px-3" style="background-color: #FFB004; border-radius: 20px; color: #000;">Read more</a>
+					</div>
+				</div>
 			</div>
-			  	<ul class="list-group">
-			    	<li class="list-group-item">Main Course &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;1000 Kkal</li>
-			  	</ul>
-			<div class="card-body">
-			    <a style="margin-left: 5px;">Asian </a>
-			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
-  			</div>
-		</div>
 
-		<div class="card">
-  			<img class="card-img-top" src="...">
-			<div class="card-body">
-			   	<h5 class="card-title" align="center">Fried Rice</h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<div class="col-lg-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/images/image1.jpg'); ?>" alt="Card image cap">
+					<div class="card-body">
+						<h3 class="card-title text-center">Fried Rice</h3>
+					</div>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-3 py-0" style="font-family: 'Roboto', sans-serif; font-weight: 500; color: #AFAFAF">
+						<p class="food-type mb-0">Main Course</p>
+						<p class="total-calory mb-0">1000 kkal</p>
+					</div>
+
+					<hr>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-4 pt-2 pb-4" style="font-family: 'Roboto', sans-serif; font-weight: 500;">
+						<p class="food-origin mb-0">Asian</p>
+						<a href="#" class="btn px-3" style="background-color: #FFB004; border-radius: 20px; color: #000;">Read more</a>
+					</div>
+				</div>
 			</div>
-			  	<ul class="list-group">
-			    	<li class="list-group-item">Main Course &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;1000 Kkal</li>
-			  	</ul>
-			<div class="card-body">
-			    <a style="margin-left: 5px;">Asian </a>
-			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
-  			</div>
-		</div>
+
+			<div class="col-lg-4">
+				<div class="card">
+					<img class="card-img-top" src="<?= base_url('assets/images/image1.jpg'); ?>" alt="Card image cap">
+					<div class="card-body">
+						<h3 class="card-title text-center">Fried Rice</h3>
+					</div>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-3 py-0" style="font-family: 'Roboto', sans-serif; font-weight: 500; color: #AFAFAF">
+						<p class="food-type mb-0">Main Course</p>
+						<p class="total-calory mb-0">1000 kkal</p>
+					</div>
+
+					<hr>
+
+					<div class="card-body d-flex justify-content-between align-items-center px-4 pt-2 pb-4" style="font-family: 'Roboto', sans-serif; font-weight: 500;">
+						<p class="food-origin mb-0">Asian</p>
+						<a href="#" class="btn px-3" style="background-color: #FFB004; border-radius: 20px; color: #000;">Read more</a>
+					</div>
+				</div>
+			</div>
 
 		</div>
-		<br>
-		<br>
-		<br>
-		</div>
-		<div class="col-md-12" style="background-color: #f7f5f0; border: 0.1px solid #f7f5f0">
-		<h2 style="color: #ffc32e; margin-top: 30px; margin-left: 40px;"> About </h2>
-		<br>
-		<div class="about">
-		<p style="	font-size: 40px; font-weight: bold; "> Did you know ? </p>
-		<br>
-		<a style="	font-size: 25px;">Nutirisi atau gizi adalah substansi organik yang dibutuhkan <br>organisme untuk fungsi normal dari sistem tubuh, pertumbuhan,<br> pemeliharaan kesehatan. Penelitian di bidang nutrisi mempelajari<br> hubungan antara makanan dan minuman</a>
-	 	<img src="<?= base_url('assets/images/about1.png'); ?>" alt="Foto about 1" style="width: 170px; position: absolute; bottom: 30px; right: 30px;">
-	 	<br>
-	 	<br>
-	 	</div>
-		 </div>
-	 	<div class="col-md-12" style="background-color: #f7f5f0; border: 0.1px solid #f7f5f0">
-	 	<div class="why">
-		<p style="	font-size: 40px; font-weight: bold; "> Why UniHealth ? </p>
-		<br>
-		<a style="	font-size: 25px;">Nutirisi atau gizi adalah substansi organik yang dibutuhkan <br>organisme untuk fungsi normal dari sistem tubuh, pertumbuhan,<br> pemeliharaan kesehatan. Penelitian di bidang nutrisi mempelajari<br> hubungan antara makanan dan minuman</a>
-	 	<img src="<?= base_url('assets/images/about2.png'); ?>" alt="Foto about 1" style="width: 170px; position: absolute; bottom: 35px; left: 120px">
-	 	<br>
-	 	<br>
+	</div>
 
-	 	
-	 	
+	<div class="col-md-12 py-5" style="background-color: #f7f5f0; border: 0.1px solid #f7f5f0">
+		<h2 class="mt-3 ml-4" style="color: #ffc32e; font-size: 40px;"> About </h2>
+
+		<div class="container">
+			<div class="fact mt-5">
+				<div class="row align-items-center">
+					<div class="col-9">
+						<p class="fact-title" style="font-family: 'Oswald', sans-serif; font-size: 35px; font-weight: 500; letter-spacing: 1px;">
+							Did you know ?
+						</p>
+						<p class="fact-explain mt-5" style="font-family: 'Crimson Text', serif; font-size: 30px;">
+							Nutrisi atau gizi adalah substansi organik yang dibutuhkan
+							organisme untuk fungsi normal dari sistem tubuh, pertumbuhan,
+							pemeliharaan kesehatan. Penelitian di bidang nutrisi mempelajari
+							hubungan antara makanan dan minuman
+						</p>
+					</div>
+
+					<div class="col-3 text-right">
+						<img src="<?= base_url('assets/images/about1.png'); ?>" alt="Foto About 1" style="height: 300px; width: 197px;">
+					</div>
+				</div>
+			</div>
+
+			<div class="why" style="margin-top: 100px;">
+				<div class="row align-items-center">
+					<div class="col-3 text-left">
+						<img src="<?= base_url('assets/images/about2.png'); ?>" alt="Foto About 1" style="height: 300px; width: 197px;">
+					</div>
+
+					<div class="col-9">
+						<p class="fact-title" style="font-family: 'Oswald', sans-serif; font-size: 35px; font-weight: 500; letter-spacing: 1px;">
+							Why UniHealth ?
+						</p>
+						<p class="fact-explain mt-5" style="font-family: 'Crimson Text', serif; font-size: 30px;">
+							Nutrisi atau gizi adalah substansi organik yang dibutuhkan 
+							organisme untuk fungsi normal dari sistem tubuh, pertumbuhan,
+							pemeliharaan kesehatan. Penelitian di bidang nutrisi mempelajari
+							hubungan antara makanan dan minuman
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -4,22 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
+ 
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
 
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/vendor/font-awesome/css/all.css'); ?>">
 
 </head>
 <body>
     <!-- Image and text -->
     
     <nav class="navbar navbar-light" style="background-color: #FFB004; font-family: 'Oswald', sans-serif; letter-spacing: 1px;">
-        <a class="navbar-brand mx-3" href="#">
+        <a class="navbar-brand mx-3" href="<?= base_url('main'); ?>">
             <img src="<?= base_url('assets/images/logo.png'); ?>" alt="UniHealth Logo" style="width: 60px; height: 60px;">
         </a>
 
@@ -37,7 +43,7 @@
                 <a class="nav-link text-dark mx-2" href="<?= base_url('auth'); ?>">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-links btn btn-dark px-3 mx-2" style="border-radius: 30px;" href="">Sign Up</a>
+                <a class="nav-links btn btn-dark px-3 mx-2" style="border-radius: 30px;" href="<?= base_url('auth/register'); ?>">Sign Up</a>
             </li>
         </ul>
     </nav>
