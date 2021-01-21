f7a1a3dfc259a1385e146876a513130d7fef5ee9
adittyakamalm <35920533+adittyakamalm@users.noreply.github.com>
2021-01-21 08:03:47 +0700

    Calculator & Search page

---
+++ application/views/search.php
@@ -0,0 +1,70 @@
+<body style="background-color: #f5f5f5">
+<div class="col-lg-6 mx-auto">
+    <div class="container">
+        <div class="logo text-center d-flex justify-content-center align-items-center mt-5">
+            <img src="<?= base_url('assets/images/logo.png'); ?>" alt="UniHealth Logo" style="width: 90px; height: 90px; ">
+            <h1 class="text-dark ml-3" style="font-family: 'Montserrat', sans-serif; font-weight: bold;">UniHealth</h1>
+        </div>
+
+        <div class="input-group" style="margin-top: 30px">
+		  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+		    aria-describedby="search-addon">
+		  <button type="button" class="btn btn-warning" style="margin-left: 20px; color: white;">Search</button>
+		</div>
+</div>
+</div>
+		<div class="d-flex justify-content-center" style="margin-top: 50px; padding-bottom: 50px;">
+		<div class="col-lg-10" style=" border: 0.1px solid #fff7de">
+		<div class="card-deck">
+		<div class="card">
+  			<img class="card-img-top" src="...">
+			<div class="card-body">
+			   	<h5 class="card-title">Fried Rice</h5>
+			   	<h6 class="card-subtitle mb-2 text-muted">Main Course</h6>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+			  	<ul class="list-group">
+			    	<li class="list-group-item">Cal. 1000kal &emsp;&emsp;&emsp;Protein 10g</li>
+			    	<li class="list-group-item">Carb. 10g &emsp;&emsp;&emsp;&emsp;Fat 10g</li>
+			  	</ul>
+			<div class="card-body">
+			    <a style="margin-left: 5px;">Asian </a>
+			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
+  			</div>
+		</div>
+
+		<div class="card">
+  			<img class="card-img-top" src="...">
+			<div class="card-body">
+			   	<h5 class="card-title">Fried Rice</h5>
+			   	<h6 class="card-subtitle mb-2 text-muted">Main Course</h6>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+			  	<ul class="list-group">
+			    	<li class="list-group-item">Cal. 1000kal &emsp;&emsp;&emsp;Protein 10g</li>
+			    	<li class="list-group-item">Carb. 10g &emsp;&emsp;&emsp;&emsp;Fat 10g</li>
+			  	</ul>
+			<div class="card-body">
+			    <a style="margin-left: 5px;">Asian </a>
+			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
+  			</div>
+		</div>
+
+		<div class="card">
+  			<img class="card-img-top" src="...">
+			<div class="card-body">
+			   	<h5 class="card-title">Fried Rice</h5>
+			   	<h6 class="card-subtitle mb-2 text-muted">Main Course</h6>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+			  	<ul class="list-group">
+			    	<li class="list-group-item">Cal. 1000kal &emsp;&emsp;&emsp;Protein 10g</li>
+			    	<li class="list-group-item">Carb. 10g &emsp;&emsp;&emsp;&emsp;Fat 10g</li>
+			  	</ul>
+			<div class="card-body">
+			    <a style="margin-left: 5px;">Asian </a>
+			    <a href="#" class="btn btn-warning" style="position: absolute; bottom: 13px; right: 20px; border: none; border-radius: 50px;">Read More</a>
+  			</div>
+		</div>
+
+		</div>
